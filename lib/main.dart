378cf29dc4da07c3f72758a451c39a85ffb60838
NicMykola <ostrikovmo1984@gmail.com>
2025-01-03 14:45:56 +0200

    Add firebase conection

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:project_energy/authorization.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: 'AIzaSyD9dzSI7e2UmxOZY-FkO4m71ytucxss9X0',
+          appId: '1:83780161459:android:03eafdf6b1f4cdaf8005c6',
+          messagingSenderId: '83780161459',
+          projectId: 'projectenergy-85a9b',
+          storageBucket: 'projectenergy-85a9b.appspot.com',
+          ));
   runApp(const MyApp());
 }
 
@@ -20,4 +30,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
