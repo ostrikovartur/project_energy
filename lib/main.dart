cb19ccc38b3df0b73f87af7a49499abe576d08ba
NicMykola <ostrikovmo1984@gmail.com>
2025-01-06 14:53:01 +0200

    Update device link

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'authorization.dart';
 import 'home.dart';
 import 'devices.dart';
 import 'device_edit.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      options: FirebaseOptions(
-          apiKey: 'AIzaSyD9dzSI7e2UmxOZY-FkO4m71ytucxss9X0',
-          appId: '1:83780161459:android:03eafdf6b1f4cdaf8005c6',
-          messagingSenderId: '83780161459',
-          projectId: 'projectenergy-85a9b',
-          storageBucket: 'projectenergy-85a9b.appspot.com',
-          ));
+    options: FirebaseOptions(
+      apiKey: 'AIzaSyD9dzSI7e2UmxOZY-FkO4m71ytucxss9X0',
+      appId: '1:83780161459:android:03eafdf6b1f4cdaf8005c6',
+      messagingSenderId: '83780161459',
+      projectId: 'projectenergy-85a9b',
+      storageBucket: 'projectenergy-85a9b.appspot.com',
+    ),
+  );
   runApp(const MyApp());
 }
 
@@ -34,10 +35,23 @@ class MyApp extends StatelessWidget {
         '/authorization': (context) => Authorization(),
         '/home': (context) => Home(),
         '/devices': (context) => Devices(),
-        '/device_edit': (context) => DeviceEdit(),
       },
-      home: Scaffold(
-      ),
+      onGenerateRoute: (settings) {
+        if (settings.name == '/device_edit') {
+          final args = settings.arguments as Map<String, dynamic>;
+          return MaterialPageRoute(
+            builder: (context) {
+              return DeviceEdit(
+                deviceId: args['deviceId'],
+                deviceName: args['deviceName'],
+                consumptionPerHour: args['consumptionPerHour'],
+              );
+            },
+          );
+        }
+        return null;
+      },
+      home: Scaffold(),
     );
   }
 }
